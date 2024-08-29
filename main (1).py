12e01a064b89773284db9ecc93cad3663d69f2f3
Gamenonno <ryanh1940@gmail.com>
2024-08-29 11:37:20 -0400

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,18 @@
+name = "Georgia"
+age = "3.5"
+hair_color = "brown"
+
+print (name)
+
+def hair_color_function():
+  if hair_color == "yellow":
+    print("You have yellow hair")
+  else:
+    print("You don't have yellow hair")
+    
+hair_color_function()
+
+
+
+sentencemerge = name + " " + "is" + " " + age + " " + "years old."
+print(sentencemerge)
